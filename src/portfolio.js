ec94a6d2079e7656dd2d28ce23d8692008cca8ec
YATHAM <yatham024@gmail.com>
2024-07-17 00:30:12 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,16 +1,12 @@
 const header = {
-  // all the properties are optional - can be left empty or deleted
-  // homepage: 'https://rjshkhr.github.io/cleanfolio',
   title: 'YS.'
 }
 
 const about = {
-  // all the properties are optional - can be left empty or deleted
   name: 'Shaikjellu Yatham Shavali',
   role: 'Full Stack Developer',
   description:
     "I'm a dedicated full-stack developer specializing in frontend technologies with 2+ years of experience,1+ years of internship experience. I excel in crafting responsive web interfaces using React, complemented by backend proficiency in Node.js and MongoDB, focusing on UI/UX design and performance optimization. Passionate about innovation, I thrive on overcoming challenges to deliver intuitive solutions that elevate user experiences and achieve business goals. Continuously learning and adapting to new trends, I'm committed to creating impactful web applications.",
-  // resume: 'https://example.com',
   social: {
     linkedin: 'https://www.linkedin.com/in/yatham024/',
     github: 'https://github.com/YATHAMSHA',
@@ -18,15 +14,12 @@ const about = {
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
   {
     name: 'CryptoTracker-app',
     description:
       'Designed and developed a responsive Crypto Tracker SPA using React for the frontend and Node.js with Express for the backend. The application fetches real-time cryptocurrency data from CoinMarketCap API, displaying prices and trends with interactive charts. Built for simplicity and usability, with a clean UI and smooth navigation.',
     stack: ['React.js', 'Node.js', 'Express.js'],
     sourceCode: 'https://github.com/YATHAMSHA/CryptoTracker-app',
-    // livePreview: 'https://github.com',
   },
   {
     name: 'Resume-builder',
@@ -46,8 +39,6 @@ const projects = [
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
   'HTML5',
   'CSS3',
   'JavaScript',
@@ -67,7 +58,6 @@ const skills = [
 ]
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
   email: 'yatham024@gmail.',
 }
 
